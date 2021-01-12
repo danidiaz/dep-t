c9ede79377fad93e2f778a30bf20159da7f3d02d
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-12 22:05:46 +0100

    MonadUnliftIO

--- lib/Control/Monad/Dep.hs
+++ lib/Control/Monad/Dep.hs
@@ -1,3 +1,18 @@
-{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE StandaloneKindSignatures, DeriveFunctor, GeneralizedNewtypeDeriving #-}
 module Control.Monad.Dep where
+
+import Data.Kind (Type)
+import Control.Monad.Trans.Class
+import Control.Monad.Reader
+import Control.Monad.IO.Unlift
+
+type DepT :: ((Type -> Type) -> Type) -> (Type -> Type) -> Type -> Type
+newtype DepT env m r = DepT { _runDepT :: ReaderT (env (DepT env m)) m r } 
+    deriving (Functor,Applicative,Monad,MonadIO,MonadUnliftIO,MonadReader (env (DepT env m)))
+
+instance MonadTrans (DepT env) where
+    lift = DepT . lift
+
+runDepT :: DepT env m r -> env (DepT env m) -> m r
+runDepT  = runReaderT . _runDepT
 
