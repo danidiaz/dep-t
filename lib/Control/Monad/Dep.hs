1a52533dcc363fc3c165226ff1faf434a0f0daff
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-12 22:21:49 +0100

    passthrough instances

--- lib/Control/Monad/Dep.hs
+++ lib/Control/Monad/Dep.hs
@@ -1,18 +1,42 @@
-{-# LANGUAGE StandaloneKindSignatures, DeriveFunctor, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Control.Monad.Dep where
 
-import Data.Kind (Type)
-import Control.Monad.Trans.Class
-import Control.Monad.Reader
+import Control.Applicative
+import Control.Monad.Zip
 import Control.Monad.IO.Unlift
+import Control.Monad.Reader
+import Control.Monad.Trans.Class
+import Control.Monad.State.Class
+import Control.Monad.Writer.Class
+import Control.Monad.Error.Class
+import Control.Monad.Cont.Class
+import Data.Kind (Type)
 
-type DepT :: ((Type -> Type) -> Type) -> (Type -> Type) -> Type -> Type
-newtype DepT env m r = DepT { _runDepT :: ReaderT (env (DepT env m)) m r } 
-    deriving (Functor,Applicative,Monad,MonadIO,MonadUnliftIO,MonadReader (env (DepT env m)))
+type DepT ::
+  ((Type -> Type) -> Type) ->
+  (Type -> Type) ->
+  Type ->
+  Type
+newtype DepT env m r = DepT {_runDepT :: ReaderT (env (DepT env m)) m r}
+  deriving (Functor, 
+                Applicative, 
+                Alternative, Monad, MonadFix, MonadFail, MonadZip, MonadPlus, MonadCont, MonadIO, MonadUnliftIO, MonadReader (env (DepT env m)))
 
 instance MonadTrans (DepT env) where
-    lift = DepT . lift
+  lift = DepT . lift
+
+deriving instance MonadState s m => MonadState s (DepT env m)
+
+deriving instance MonadWriter w m => MonadWriter w (DepT env m)
+
+deriving instance MonadError e m => MonadError e (DepT env m)
 
 runDepT :: DepT env m r -> env (DepT env m) -> m r
-runDepT  = runReaderT . _runDepT
-
+runDepT = runReaderT . _runDepT
