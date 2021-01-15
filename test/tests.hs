c3cd89456ddacaa9ed2ac79c23867d12368dcf21
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-15 20:20:34 +0100

    added a new bean in tests

--- test/tests.hs
+++ test/tests.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE RankNTypes #-}
 
 module Main (main) where
 
@@ -13,14 +14,15 @@ import Control.Monad.Reader
 import Data.Kind
 import Rank2 qualified
 import Rank2.TH qualified
+import Prelude hiding (log)
 
 -- The environment doesn't know about any concrete monad
 type Env :: (Type -> Type) -> Type
 data Env m = Env
-  { logger :: String -> m (),
-    logic :: Int -> m Int
+  { _logger :: String -> m (),
+    _repository :: Int -> m (),
+    _controller :: Int -> m Int
   }
-
 $(Rank2.TH.deriveFunctor ''Env)
 
 -- Has-style typeclasses can be provided to avoid depending on concrete
@@ -28,69 +30,71 @@ $(Rank2.TH.deriveFunctor ''Env)
 -- Note that the environment determines the monad.
 type HasLogger :: Type -> (Type -> Type) -> Constraint
 class HasLogger r m | r -> m where
-  getLogger :: r -> String -> m ()
+  logger :: r -> String -> m ()
 
 -- If our environment is parmeterized by the monad m, then logging is done in
 -- m.
 instance HasLogger (Env m) m where
-  getLogger = logger
+  logger = _logger
+
+type HasRepository :: Type -> (Type -> Type) -> Constraint
+class HasRepository r m | r -> m where
+  repository :: r -> Int -> m ()
+
+instance HasRepository (Env m) m where
+  repository = _repository
 
 -- These two functions don't know the concrete envionment record.
 --
 -- This one because it only needs MonadIO.
-_logger :: MonadIO m => String -> m ()
-_logger msg = liftIO (putStrLn msg)
+mkStdoutLogger :: MonadIO m => String -> m ()
+mkStdoutLogger msg = liftIO (putStrLn msg)
+
+mkRepository :: (MonadReader e m, HasLogger e m) => Int -> m () 
+mkRepository entity = do
+  doLog <- reader logger
+  doLog "I'm going to write the entity!"
 
 -- This one because it receives a getter for the logger
--- A HasX-style typeclass would have been an alternative. 
-_logic :: MonadReader e m => (e -> String -> m ()) -> Int -> m Int
-_logic getLogger x = do
-  logger <- reader getLogger
-  logger "I'm going to multiply a number by itself!"
+-- A HasX-style typeclass would have been an alternative.
+mkController :: (MonadReader e m, HasLogger e m, HasRepository e m) => Int -> m Int
+mkController x = do
+  doLog <- reader logger
+  doLog "I'm going to insert in the db!"
+  insert <- reader repository
+  insert x
   return $ x * x
 
 -- This is the first time DepT is used in this module.
 -- Note that it is only here where we settle for IO.
 env :: Env (DepT Env IO)
 env =
-  Env
-    { logger = _logger,
-      logic = _logic logger
-    }
+  let _logger = mkStdoutLogger
+      _controller = mkController
+      _repository = mkRepository
+   in Env {_logger, _controller, _repository}
 
--- We select "logic" as the entrypoint and run it.
+-- We select "controller" as the entrypoint and run it.
 result :: IO Int
-result = runDepT (logic env 7) env
-
--- An attempt with ReaderT which doesn't work
--- env' =
---   Env
---     { logger = __logic logger
---     }
--- 
--- result' :: IO Int
--- result' = runReaderT (logic env' 7) env'
-
+result = runDepT (_controller env 7) env
 
 -- The environment doesn't know about any concrete monad
 type BiggerEnv :: (Type -> Type) -> Type
 data BiggerEnv m = BiggerEnv
-  { inner :: Env m,
-    extra :: Int -> m Int
+  { _inner :: Env m,
+    _extra :: Int -> m Int
   }
-
 $(Rank2.TH.deriveFunctor ''BiggerEnv)
 
 biggerEnv :: BiggerEnv (DepT BiggerEnv IO)
-biggerEnv = BiggerEnv 
-    {
-        -- inner = (Rank2.<$>) (withDepT (Rank2.<$>) inner) env,
-        inner = zoomEnv (Rank2.<$>) inner env,
-        extra = pure 
-    }
+biggerEnv =
+  let -- _inner = (Rank2.<$>) (withDepT (Rank2.<$>) inner) env,
+      _inner' = zoomEnv (Rank2.<$>) _inner env
+      _extra = pure
+   in BiggerEnv {_inner = _inner', _extra}
 
 main :: IO ()
 main = do
-    r <- runDepT ((logic . inner $ biggerEnv) 7) biggerEnv
-    print r
+  r <- runDepT ((_controller . _inner $ biggerEnv) 7) biggerEnv
+  print r
 
