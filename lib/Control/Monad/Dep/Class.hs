e89f08deac075086e3ccfed3f332f74f9ccdf7f1
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-16 17:27:31 +0100

    Original version of the code for Call

--- lib/Control/Monad/Dep/Class.hs
+++ lib/Control/Monad/Dep/Class.hs
@@ -1,5 +1,19 @@
+{-# language MultiParamTypeClasses, TypeFamilies, FlexibleInstances #-}
 module Control.Monad.Dep.Class where
 
 import Control.Monad.Reader
 
+-- Code taken and modified from these SO answers from Li-yao Xia and dfeuer:
+-- https://stackoverflow.com/a/61648924/1364288
 
+class e ~ TheEnv r => Call e r where
+  type TheEnv r
+  call :: (e -> r) -> r
+
+instance Call e r => Call e (a -> r) where
+  type TheEnv (a -> r) = TheEnv r
+  call f x = call (\env -> f env x)
+
+instance (Monad m, e ~ e') => Call e (ReaderT e' m r) where
+  type TheEnv (ReaderT e' m r) = e'
+  call f = ask >>= f
