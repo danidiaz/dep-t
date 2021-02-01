ebcb0c8b8754a0004c4ba3bb6433b993871ec152
Daniel Diaz <diaz.carrete@facebook.com>
2021-02-01 23:33:42 +0100

    first impl of deceive

--- test/tests_curry.hs
+++ test/tests_curry.hs
@@ -10,6 +10,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Main (main) where
 
@@ -24,6 +27,48 @@ import Rank2.TH qualified
 import Test.Tasty
 import Test.Tasty.HUnit
 import Prelude hiding (log)
+import Data.SOP
+import Data.SOP.NP
+
+type Multicurryable ::
+  [Type] ->
+  ((Type -> Type) -> Type) ->
+  (Type -> Type) ->
+  Type ->
+  Type ->
+  Constraint
+class Multicurryable as e m r curried | curried -> as e m r where
+  type DownToBaseMonad as e m r curried :: Type
+  multiuncurry :: curried -> NP I as -> DepT e m r
+  multicurry :: (NP I as -> DepT e m r) -> curried
+  _runFromEnv :: m (e (DepT e m)) -> (e (DepT e m) -> curried) -> DownToBaseMonad as e m r curried
+
+instance Monad m => Multicurryable '[] e m r (DepT e m r) where
+  type DownToBaseMonad '[] e m r (DepT e m r) = m r
+  multiuncurry action Nil = action
+  multicurry f = f Nil
+  _runFromEnv producer extractor = do
+    e <- producer
+    runDepT (extractor e) e
+
+instance Multicurryable as e m r curried => Multicurryable (a ': as) e m r (a -> curried) where
+  type DownToBaseMonad (a ': as) e m r (a -> curried) = a -> DownToBaseMonad as e m r curried
+  multiuncurry f (I a :* as) = multiuncurry @as @e @m @r @curried (f a) as
+  multicurry f a = multicurry @as @e @m @r @curried (f . (:*) (I a))
+  _runFromEnv producer extractor a = _runFromEnv @as @e @m @r @curried producer (\f -> extractor f a)
+
+class Multicurryable as e m r curried => Deceivable e' as e m r curried where
+  type OutermostReader e' as e m r curried :: Type  
+  deceive :: (e (DepT e m) -> e') -> OutermostReader e' as e m r curried -> curried 
+
+instance (Monad m, Coercible e' (e (DepT e m))) => Deceivable e' '[] e m r (DepT e m r) where
+  type OutermostReader e' '[] e m r (DepT e m r) = ReaderT e' m r
+  deceive f action = DepT (withReaderT f action)
+
+instance Deceivable e' as e m r curried => Deceivable e' (a ': as) e m r (a -> curried) where
+  type OutermostReader e' (a ': as) e m r (a -> curried) = a -> OutermostReader e' as e m r curried
+  deceive f g a = deceive @e' @as @e @m @r f (g a)
+
 
 tests :: TestTree
 tests =
