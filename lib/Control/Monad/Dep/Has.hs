d2dee645363e25c195afcebfc55434de441ca75d
Daniel Diaz <diaz.carrete@facebook.com>
2021-02-06 14:10:48 +0100

    formatting

--- lib/Control/Monad/Dep/Has.hs
+++ lib/Control/Monad/Dep/Has.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 
-module Control.Monad.Dep.Has where
+module Control.Monad.Dep.Has (Has (..), DepMarker (..)) where
 
 import Data.Kind
 import GHC.Records
@@ -18,15 +18,15 @@ import GHC.TypeLits
 
 type Has :: k -> (Type -> Type) -> Type -> Constraint
 class Has k d e | e -> d where
-    type The k d e :: Type
-    type The k d e = ExtractedDepType k d
-    the :: e -> The k d e
-    default the :: (DepMarker k d, HasField (PreferredFieldName k d) e (The k d e)) => e -> The k d e
-    the = getField @(PreferredFieldName k d)
+  type The k d e :: Type
+  type The k d e = ExtractedDepType k d
+  the :: e -> The k d e
+  default the :: (DepMarker k d, HasField (PreferredFieldName k d) e (The k d e)) => e -> The k d e
+  the = getField @(PreferredFieldName k d)
 
 type DepMarker :: k -> (Type -> Type) -> Constraint
 class DepMarker k d where
-    -- The Char kind would be useful here, to lowercase the first letter of the 
-    -- k type and use it as the default preferred field name.
-    type PreferredFieldName k d :: Symbol
-    type ExtractedDepType k d :: Type
+  -- The Char kind would be useful here, to lowercase the first letter of the
+  -- k type and use it as the default preferred field name.
+  type PreferredFieldName k d :: Symbol
+  type ExtractedDepType k d :: Type
