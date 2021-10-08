dd58958b4d22ce6c232543e54ad5102fb7f94db5
Daniel Diaz <diaz.carrete@facebook.com>
2021-10-09 01:16:16 +0200

    ...

--- lib/Control/Monad/Dep/Has.hs
+++ lib/Control/Monad/Dep/Has.hs
@@ -63,12 +63,57 @@ module Control.Monad.Dep.Has (
         -- * Component defaults
     ,   Dep (..)
         -- * call helper
+    ,   asCall
     ,   useCall
     ) where
 
-import DI
+import Data.Kind
+import GHC.Records
+import GHC.TypeLits
+import Data.Coerce
+import Control.Monad.Reader
 import Control.Monad.Dep.Class
-import Control.Monad.Reader.Class
+
+-- | A generic \"Has\" class. When partially applied to a parametrizable
+-- record-of-functions @r_@, produces a 2-place constraint that can be later
+-- used with "Control.Monad.Dep.Class".
+type Has :: ((Type -> Type) -> Type) -> (Type -> Type) -> Type -> Constraint
+class Has r_ m env | env -> m where
+  -- |  Given an environment @e@, produce a record-of-functions parameterized by the environment's effect monad @d@.
+  --
+  -- The hope is that using a selector function on the resulting record will
+  -- determine its type without the need for type annotations.
+  --
+  -- (This will likely not play well with RecordDotSyntax. See also <https://chrisdone.com/posts/import-aliases-field-names/ this trick>.)
+  dep :: env -> r_ m
+  default dep :: (Dep r_, HasField (DefaultFieldName r_) env u, Coercible u (r_ m)) => env -> r_ m
+  dep env = coerce . getField @(DefaultFieldName r_) $ env
+
+asCall :: forall env m . env -> forall r_ x. Has r_ m env => (r_ m -> x) -> x
+asCall env = \f -> f (dep env)
+
+-- | Avoids repeated calls to 'liftD' when all the effects in a function come
+--   from the environment.
+--
+--   Similar to 'useEnv', but the callback receives a \"call\" function that
+--   pre-applies any record selector with the correct record extracted from the
+--   environment.
+useCall :: forall d env m y . (LiftDep d m, MonadReader env m) => ((forall r_ x . Has r_ d env => (r_ d -> x) -> x) -> d y) -> m y
+useCall usesCall = do
+  (asCall -> call) <- ask @env
+  liftD (usesCall call)
+
+-- | Parametrizable records-of-functions can be given an instance of this
+-- typeclass to specify the default field name 'Has' expects for the component
+-- in the environment record.
+--
+-- This allows defining 'Has' instances with empty bodies, thanks to
+-- @DefaultSignatures@.
+type Dep :: ((Type -> Type) -> Type) -> Constraint
+class Dep r_ where
+  -- The Char kind would be useful here, to lowercase the first letter of the
+  -- k type and use it as the default preferred field name.
+  type DefaultFieldName r_ :: Symbol
 
 -- $setup
 --
@@ -91,14 +136,4 @@ import Control.Monad.Reader.Class
 -- >>> import GHC.Generics (Generic)
 --
 
--- | Avoids repeated calls to 'liftD' when all the effects in a function come
---   from the environment.
---
---   Similar to 'useEnv', but the callback receives a \"call\" function that
---   pre-applies any record selector with the correct record extracted from the
---   environment.
-useCall :: forall d env m y . (LiftDep d m, MonadReader env m) => ((forall r_ x . Has r_ d env => (r_ d -> x) -> x) -> d y) -> m y
-useCall usesCall = do
-  (asCall -> call) <- ask @env
-  liftD (usesCall call)
 
